9e3dbc8d8aa263659b8531b4d7f84f180b8683ee
hudsonschumaker <hudsonschumaker@192.168.0.1>
2015-11-07 15:32:14 -0200

    Docx OK

--- src/br/com/schumaker/beta/ReadDocX.java
+++ src/br/com/schumaker/beta/ReadDocX.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.schumaker.beta;
 
 import java.io.File;
 import java.io.FileInputStream;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import java.io.IOException;
+import java.util.List;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 /**
  *
@@ -19,24 +14,32 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 public class ReadDocX {
 
     public static void main(String[] args) {
-        try {
 
-            File file = new File("/users/hudsonschumaker/downloads/PROCESSO - BPCS - Dar baixa ao CDR.docx");
-            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
-            XWPFDocument doc = new XWPFDocument(fis);
-            XWPFWordExtractor ex = new XWPFWordExtractor(doc);
-           
-            
-            for (String rawText : ex.) {
-                String text = extractor.stripFields(rawText);
-                if(text.length()>10)
-                System.out.println(text.trim());
+            File file = new File("/users/hudsonschumaker/downloads/test/PROCESSO - BPCS - Dar baixa ao CDR.docx");
+            StringBuilder content = new StringBuilder();
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file.getAbsolutePath());
+                XWPFDocument document = new XWPFDocument(fis);
+                List<XWPFParagraph> paragraphs = document.getParagraphs();
+                System.out.println("Total no of paragraph " + paragraphs.size());
+                for (XWPFParagraph para : paragraphs) {
+                  //  content.append(para.toString().trim());
+                    System.out.println(para.getText().trim());
+                }
+            } catch (Exception ex) {
+                System.err.println("readWordFile::HsDocxFile: " + ex.getMessage());
+
+            } finally {
+                try {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                } catch (IOException ex) {
+                    System.err.println("readWordFile::HsDocxFile: " + ex.getMessage());
+
+                }
             }
-
-//                if(text.length()>10)
-            System.out.println(text.trim());
-
-        } catch (Exception exep) {
+          //  return content.toString();
         }
     }
-}
